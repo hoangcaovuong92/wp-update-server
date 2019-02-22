63a44a7a48d453eb70d407a87fe60611d91af77b
vuong.hoang@codespot.vn <ntchctvcdgt9>
2019-02-22 17:41:20 +0700

    update 22/2/2019

--- packages/feellio/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/loader.php';
-//require_once '../../db/wd_server_database.php';
-require_once __DIR__ . '/wd_checker_server.php';
-$server = new wd_checker_server();
-$server->handleRequest();
